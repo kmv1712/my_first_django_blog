5d6130ee1dbecc6ba3dced6c90de56334fbd378d
KarpovM <kmv1712@gmail.com>
2019-12-02 10:30:16 +0500

    Добавил Post, Tag, Images  в админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models her.
+
+from .models import Post, Tag, Images
+
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Images)
+
